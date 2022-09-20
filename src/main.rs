8589636212ec9298689e99daf3c00fbf2b4e151a
GrauKatze <GrauKatze@protonmail.com>
2022-09-20 20:38:04 +0300

    refactor server

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,12 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
+mod server;
+use std::net::{IpAddr, Ipv4Addr};
+use crate::server::Server;
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-         for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-
-    }
+    let (a,b,c,d) = (127,0,0,1);
+    let server = Server{
+        addr: IpAddr::V4(Ipv4Addr::new(a,b,c,d)),
+        port: 7878,
+    };
+    Server::start_server(&server);
 }
 
-
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("views/index.html").unwrap();
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
-        } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("views/404.html").unwrap();
-        let length = contents.len();
-
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-}
---
+++ src/server.rs
@@ -0,0 +1,42 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream, SocketAddr, IpAddr},
+};
+pub struct Server{
+    pub addr: IpAddr,
+    pub port: u16,
+}
+impl Server{
+    pub fn start_server(&self){
+        let socket = SocketAddr::new(self.addr,self.port );
+
+
+        let listener = TcpListener::bind(socket).unwrap();
+        println!("server start on host {} and port {}", self.addr, self.port);
+
+        for stream in listener.incoming(){
+            let stream = stream.unwrap();
+            Self::handle_connection(stream);
+            println!("new connection!");
+        }
+    }
+    
+    fn handle_connection(mut stream: TcpStream) {
+        let buf_reader = BufReader::new(&mut stream);
+        let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+        let (status_line, filename) = if request_line == "GET / HTTP/1.1"{
+            ("HTTP/1.1 200 OK", "views/index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "views/404.html")
+        };
+
+        let contents = fs::read_to_string(filename).unwrap();
+        let lenght = contents.len();
+
+        let response = 
+            format!("{status_line}\r\nContent-Lenght: {lenght}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+}
